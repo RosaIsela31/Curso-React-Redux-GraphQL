a0fb45ca22390044e1e5aec0c258d56b74acd2f6
Rosa <rosaislp@gmail.com>
2020-07-13 12:17:47 -0500

    creating an action to save favorites characters

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { removeCharacterAction } from '../../redux/charactersDuck';
+import { removeCharacterAction, addToFavoritesAction } from '../../redux/charactersDuck';
 import Card from '../card/Card';
 import styles from './home.module.css';
 
-function Home({ characters, removeCharacterAction }) {
+function Home({ characters, removeCharacterAction, addToFavoritesAction }) {
+    function addToFavorites() {
+        addToFavoritesAction();
+    };
     
     function renderCharacter() {
         let char = characters[0]
-        return (<Card leftClick={nextCharacter} {...char} />)
+        return (<Card rightClick={addToFavorites} leftClick={nextCharacter} {...char} />)
     };
 
     function nextCharacter () {
@@ -31,4 +34,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, { removeCharacterAction })(Home)
+export default connect(mapStateToProps, { removeCharacterAction, addToFavoritesAction })(Home)
