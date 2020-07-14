c27dd5e98411c8edf6639b2c04cdf6dfb2ccb488
Rosa <rosaislp@gmail.com>
2020-07-13 19:26:46 -0500

    back only to redux and db from firebase, remove graphql

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-// import Home from './components/home/HomePage';
+import Home from './components/home/HomePage';
 import FavPage from './components/favs/FavPage';
 import LoginPage from './components/login/LoginPage';
-import GraphHome from './components/home/GraphHome';
+// import GraphHome from './components/home/GraphHome';
 
 const PrivateRoute = ({ path, component, ...rest }) => {
     let storage = localStorage.getItem('storage');
@@ -19,7 +19,7 @@ const PrivateRoute = ({ path, component, ...rest }) => {
 export default function Routes() {
     return (
         <Switch>
-            <PrivateRoute exact path="/" component={GraphHome} />
+            <PrivateRoute exact path="/" component={Home} />
             <PrivateRoute path="/favs" component={FavPage} />
             <Route path="/login" component={LoginPage} />
         </Switch>
