8fb9788e3a8e2a48c41f464398c69e4e7193b3be
Rosa <rosaislp@gmail.com>
2020-07-13 20:41:47 -0500

    Updates

---
+++ precache-manifest.b955e8a81af622fb457916a6f4154aa7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099a8e36f94e3539f84434f8a8b627f8",
+    "url": "/Curso-React-Redux-GraphQL/index.html"
+  },
+  {
+    "revision": "3b40ec24fd9e98a7ea1f",
+    "url": "/Curso-React-Redux-GraphQL/static/css/2.a751895c.chunk.css"
+  },
+  {
+    "revision": "5d7eff6f74ae83e8c53b",
+    "url": "/Curso-React-Redux-GraphQL/static/css/main.59500ea7.chunk.css"
+  },
+  {
+    "revision": "3b40ec24fd9e98a7ea1f",
+    "url": "/Curso-React-Redux-GraphQL/static/js/2.03195307.chunk.js"
+  },
+  {
+    "revision": "926ff523fdd3c5460cfa092ccd8b1b3d",
+    "url": "/Curso-React-Redux-GraphQL/static/js/2.03195307.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d7eff6f74ae83e8c53b",
+    "url": "/Curso-React-Redux-GraphQL/static/js/main.7b92c927.chunk.js"
+  },
+  {
+    "revision": "764e0b072931936fad57",
+    "url": "/Curso-React-Redux-GraphQL/static/js/runtime-main.ff6c9031.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Curso-React-Redux-GraphQL/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Curso-React-Redux-GraphQL/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Curso-React-Redux-GraphQL/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Curso-React-Redux-GraphQL/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Curso-React-Redux-GraphQL/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
